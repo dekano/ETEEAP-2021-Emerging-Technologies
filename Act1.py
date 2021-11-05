d9f7d775c7a85cb335a765b6c0ca1d2e0cd29249
Dean Justin Cruz <deanjustin17@gmail.com>
2021-11-05 15:27:29 +0800

    Primary Code

---
+++ Act1.py
@@ -0,0 +1,10 @@
+
+preLims = int(input("Prelims: "))
+midTerm = int(input("Midterm: "))
+semiFinal = int(input("Semifinal: "))
+finals = int(input("Final: "))
+
+Ave = float((preLims + midTerm + semiFinal + finals) / 4)
+
+print(Ave)
+
