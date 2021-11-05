9aed2fad86aa1a1221f39e72fadc92231eca943b
Dean Justin Cruz <deanjustin17@gmail.com>
2021-11-05 16:04:16 +0800

    added equivalent grade of the given average

--- Act1.py
+++ Act1.py
@@ -6,8 +6,17 @@ finals = int(input("Final: "))
 
 Ave = float((preLims + midTerm + semiFinal + finals) / 4)
 
-if (Ave >= 75):
-    print("PASSED")
-else: 
-    print("FAILED")
+if (Ave < 60):
+    print("Your grade is E")
+elif(Ave >= 61 or Ave <= 70):
+    print("Your grade is E")
+elif(Ave >= 71 or Ave <= 79):
+    print("Your grade is D")
+elif(Ave >= 80 or Ave <= 89):
+    print("Your grade is C")
+elif(Ave >= 90 or Ave <= 98):
+    print("Your grade is B")
+elif(Ave > 99):
+    print("Your grade is A")
+
 
